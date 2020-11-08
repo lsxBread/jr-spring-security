463deeeb9c0b88d7368a2ec14582931ee2511ef3
Lydia Zhang <leo.liu@vudoo.com>
2020-11-08 14:43:48 +1100

    add User and Authority entity

---
+++ src/main/java/com/example/jrspringsecurity/entities/Authority.java
@@ -0,0 +1,29 @@
+package com.example.jrspringsecurity.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@ToString(exclude = "users")
+@Table(name = "authorities")
+public class Authority {
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @Column(name = "id",updatable = false, nullable = false)
+    private Long id;
+
+    @Column(name = "permission", unique = true, nullable = false)
+    private String permission;
+
+    @ManyToMany
+    @JoinTable(name = "users_authorities",
+            joinColumns = @JoinColumn(name = "authority_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> users;
+}
