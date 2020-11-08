b4a44fdeffc79208afb9f4524ccc563e389965f4
Lydia Zhang <leo.liu@vudoo.com>
2020-11-08 16:44:37 +1100

    add JwtConfig used to project config data in application.properties into object

---
+++ src/main/java/com/example/jrspringsecurity/jwt/JwtConfig.java
@@ -0,0 +1,32 @@
+package com.example.jrspringsecurity.jwt;
+
+import com.google.common.net.HttpHeaders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Data
+@Component
+@Configuration
+@ConfigurationProperties(prefix = "application.jwt")
+public class JwtConfig {
+
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+
+
+    public String getAuthorizationHeader() {
+        return HttpHeaders.AUTHORIZATION;
+    }
+
+    @Bean
+    public SecretKey secretKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+}
