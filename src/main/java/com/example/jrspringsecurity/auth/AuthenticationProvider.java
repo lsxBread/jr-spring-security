7267b40abe3763582df92406e2166a929e7c15f2
Lydia Zhang <leo.liu@vudoo.com>
2020-11-08 16:52:34 +1100

    config jwt into ApplicationSecurityConfig

---
+++ src/main/java/com/example/jrspringsecurity/auth/AuthenticationProvider.java
@@ -0,0 +1,24 @@
+package com.example.jrspringsecurity.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AuthenticationProvider {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private ApplicationUsersService applicationUsersService;
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUsersService);
+        return provider;
+    }
+}
--- src/main/java/com/example/jrspringsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/jrspringsecurity/security/ApplicationSecurityConfig.java
@@ -1,24 +1,25 @@
 package com.example.jrspringsecurity.security;
 
 import com.example.jrspringsecurity.auth.ApplicationUsersService;
+import com.example.jrspringsecurity.jwt.JwtConfig;
+import com.example.jrspringsecurity.jwt.JwtTokenVerifier;
+import com.example.jrspringsecurity.jwt.JwtUsernameAndPasswordAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import javax.crypto.SecretKey;
 
 import static com.example.jrspringsecurity.security.ApplicationUserRole.*;
-import static com.example.jrspringsecurity.security.ApplicationUserAuthority.*;
 
 @EnableWebSecurity
 @Configuration
@@ -31,29 +32,33 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private ApplicationUsersService applicationUsersService;
 
+    @Autowired
+    private SecretKey secretKey;
+
+    @Autowired
+    private  JwtConfig jwtConfig;
+
+    @Autowired
+    private DaoAuthenticationProvider daoAuthenticationProvider;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig),JwtUsernameAndPasswordAuthenticationFilter.class)
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
+                .authenticated();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(daoAuthenticationProvider());
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(passwordEncoder);
-        provider.setUserDetailsService(applicationUsersService);
-        return provider;
+        auth.authenticationProvider(daoAuthenticationProvider);
     }
 }
